9c2cff6d4c0cee7952b428b7afe12176047100b6
daiki-kanai <knidik3@gmail.com>
2020-06-21 14:05:05 +0900

    onlyFirstKeyword

--- wikipedia-tree.rb
+++ wikipedia-tree.rb
@@ -14,13 +14,44 @@ end
 # htmlをパースしてオブジェクトを作成
 document = Nokogiri::HTML.parse(html, nil, charset)
 
-# 検索キーワードの抽出と出力
-keyword = document.xpath('//h1').inner_text
+# 概要の箇所を抽出し、keywordに代入
+first_keyword = document.xpath('//h1').inner_text
+
+# tree = []
+# tree << keyword
 
 # 最初のpタグを抽出し、その中のaタグを出力
-next_link = document.xpath('//div[@class="mw-parser-output"]/p').first.xpath(".//a")
+second_link = document.xpath('//div[@class="mw-parser-output"]/p').first.xpath('.//a')
 
 # 最初のキーワードとそのリンク先を結合して出力
-next_link.each do |word|
-  puts "#{keyword} ー #{word.inner_text}"
+# next_link.each do |word|
+#   puts "#{keyword} ー #{word.inner_text}"
+
+#   @new_link = "https://ja.wikipedia.org/#{word.attribute('href').value}"
+# end
+
+puts "#{first_keyword} ー #{second_link.first.inner_text}"
+
+new_link = "https://ja.wikipedia.org/#{second_link.attribute('href').value}"
+
+
+
+# 2周目
+
+url = new_link
+# 取れてきてるか確認
+# puts url
+
+charset = nil
+html = open(url) do |f|
+  charset = f.charset # 文字種別を取得
+  f.read # htmlを読み込んで変数htmlに渡す
 end
+
+# htmlをパースしてオブジェクトを作成
+document = Nokogiri::HTML.parse(html, nil, charset)
+
+# 最初のpタグを抽出し、その中のaタグを出力
+third_link = document.xpath('//div[@class="mw-parser-output"]/p').first.xpath('.//a')
+
+puts "#{first_keyword} - #{second_link.first.inner_text} - #{third_link.first.inner_text}"
