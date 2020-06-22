071994cfc87cb1ecd951d66893d6bac99101a92b
daiki-kanai <knidik3@gmail.com>
2020-06-22 15:01:00 +0900

    useArray

--- wikipedia-tree.rb
+++ wikipedia-tree.rb
@@ -17,23 +17,20 @@ document = Nokogiri::HTML.parse(html, nil, charset)
 # 概要の箇所を抽出し、keywordに代入
 first_keyword = document.xpath('//h1').inner_text
 
-# tree = []
-# tree << keyword
+node1 = []
+node1 << first_keyword
+# puts node1
 
 # 最初のpタグを抽出し、その中のaタグを出力
 second_link = document.xpath('//div[@class="mw-parser-output"]/p').first.xpath('.//a')
 
-# 最初のキーワードとそのリンク先を結合して出力
-# next_link.each do |word|
-#   puts "#{keyword} ー #{word.inner_text}"
-
-#   @new_link = "https://ja.wikipedia.org/#{word.attribute('href').value}"
-# end
-
-puts "#{first_keyword} ー #{second_link.first.inner_text}"
+# puts "#{first_keyword} ー #{second_link.first.inner_text}"
+node2 = []
+node2 << second_link.first.inner_text
+node1 << node2
+# puts node1
 
 new_link = "https://ja.wikipedia.org/#{second_link.attribute('href').value}"
-
 
 
 # 2周目
@@ -54,4 +51,27 @@ document = Nokogiri::HTML.parse(html, nil, charset)
 # 最初のpタグを抽出し、その中のaタグを出力
 third_link = document.xpath('//div[@class="mw-parser-output"]/p').first.xpath('.//a')
 
-puts "#{first_keyword} - #{second_link.first.inner_text} - #{third_link.first.inner_text}"
+# puts "#{first_keyword} - #{second_link.first.inner_text} - #{third_link.first.inner_text}"
+node3 = []
+node3 << third_link.first.inner_text
+node2 << node3
+# puts node1
+
+new_link = "https://ja.wikipedia.org/#{third_link.attribute('href').value}"
+
+
+# 3周目
+url = new_link
+
+charset = nil
+html = open(url) do |f|
+  charset = f.charset # 文字種別を取得
+  f.read # htmlを読み込んで変数htmlに渡す
+end
+
+document = Nokogiri::HTML.parse(html, nil, charset)
+forth_link = document.xpath('//div[@class="mw-parser-output"]/p').first.xpath('.//a')
+node4 = []
+node4 << forth_link.first.inner_text
+node3 << node4
+puts node1
